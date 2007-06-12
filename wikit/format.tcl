6590e3848922266c42429245fadfb590c9665ae6
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2007-06-12 15:33:38 +0000

    * fixups for history - format incorrect _ref

--- wikit/format.tcl
+++ wikit/format.tcl
@@ -1187,7 +1187,7 @@ namespace eval Wikit::Format {
           foreach {page version who when} [split $text ";"] break
           append result "\n<div class='annotated'>\n"
           append result "  <span class='versioninfo'>\n"
-          append result "    <span class='versionnum'><a href='/_rev/$page?V=$version&A=1'>$version</a></span>\n"
+          append result "    <span class='versionnum'><a href='/_revision/$page?V=$version&A=1'>$version</a></span>\n"
           append result "    <span class='versionwho'>$who</span><br>\n"
           append result "    <span class='versiondate'>$when</span>\n"
           append result "  </span>\n"
