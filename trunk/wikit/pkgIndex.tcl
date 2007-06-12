d3eb2c6dbfe416c685b3a67b715f483bc2e9481b
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2007-06-12 04:49:34 +0000

    * added kbk's changes for history

--- trunk/wikit/pkgIndex.tcl
+++ trunk/wikit/pkgIndex.tcl
@@ -5,7 +5,7 @@ package ifneeded Wikit 1.1 [list source [file join $dir wikit.tcl]]
 package ifneeded Wikit::Format 1.1 [list source [file join $dir format.tcl]]
 package ifneeded Wikit::Gui 1.1 [list source [file join $dir gui.tcl]]
 
-package ifneeded Wikit::Db 1.1 [list source [file join $dir db.tcl]]
+package ifneeded Wikit::Db 1.2 [list source [file join $dir db.tcl]]
 package ifneeded Wikit::Cache 1.0 [list source [file join $dir cache.tcl]]
 package ifneeded Wikit::Image 1.0 [list source [file join $dir image.tcl]]
 package ifneeded Wikit::Lock 1.0 [list source [file join $dir lock.tcl]]
