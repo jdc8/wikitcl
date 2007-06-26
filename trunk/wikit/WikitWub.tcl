bea8257fc61efdf8954010717806047dde456f7c
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2007-06-26 01:34:39 +0000

    * Problem with Caching Created pages

--- trunk/wikit/WikitWub.tcl
+++ trunk/wikit/WikitWub.tcl
@@ -770,7 +770,7 @@ namespace eval WikitWub {
 	    }
 	}
 
-	return [Http NoCache [Http Created $r $R <p>[<a> href $R "Created Account"]</p>]]
+	return [Http Created $r $R <p>[<a> href $R "Created Account"]</p>]
     }
 
     proc who {r} {
@@ -885,7 +885,7 @@ namespace eval WikitWub {
 		    # this is a ghostly conflict-with-self - log and ignore
 		    Debug.error "Conflict on Edit of $N: '$O' ne '[list $date $who]' at date $when"
 		    set url http://[dict get $r host]/$N
-		    return [Http NoCache [Http Created $r $url <p>[<a> href $url "Edited Page"]</p>]]
+		    return [Http Created $r $url <p>[<a> href $url "Edited Page"]</p>]
 		} else {
 		    set X [list $date $who]
 		    variable conflict
@@ -935,7 +935,7 @@ namespace eval WikitWub {
 	    }
 	}
 	set url http://[dict get $r host]/$N
-	return [Http NoCache [Http Created $r $url <p>[<a> href $url "Edited Page"]</p>]]
+	return [Http Created $r $url <p>[<a> href $url "Edited Page"]</p>]
     }
 
     proc GetPage {id} {
@@ -1006,7 +1006,7 @@ namespace eval WikitWub {
 	if {$R eq ""} {
 	    set R http://[dict get $r host]/4
 	}
-	return [Http NoCache [Http Created $r $R <p>[<a> href $R "Loaded MOTD"]</p>]]
+	return [Http Created $r $R <p>[<a> href $R "Loaded MOTD"]</p>]
     }
 
 	# called to generate a page with references
@@ -1567,7 +1567,6 @@ proc incoming {req} {
 	do convert do $response	;# convert page
 	dict set response -transaction [dict get $request -transaction]
 	dict set response -generation [dict get $request -generation]
-
 	::thread::send -async [dict get $request -worker] [list send $response]
 	set request [dict create]	;# go idle
     }
