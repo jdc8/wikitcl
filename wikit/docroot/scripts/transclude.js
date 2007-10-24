0008ecc4074d151d84e49437b4f874e0b479e7b2
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2007-10-24 02:33:19 +0000

    Major Upgrade to support TOC and new CSS

--- wikit/docroot/scripts/transclude.js
+++ wikit/docroot/scripts/transclude.js
@@ -2,42 +2,10 @@
  * Dynamic Ajax Content- © Dynamic Drive DHTML code library (www.dynamicdrive.com)
  * This notice MUST stay intact for legal use
  * Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
- * Functions taken from Dynamic Ajax Content:
+ * Based on functions taken from Dynamic Ajax Content:
  *    ajaxpage
  *    loadpage
  ***********************************************/
-    
-function ajaxpage(url, containerid){
-    var page_request = false
-    if (window.XMLHttpRequest) // if Mozilla, Safari etc
-        page_request = new XMLHttpRequest()
-    else if (window.ActiveXObject){ // if IE
-	try {
-	    page_request = new ActiveXObject("Msxml2.XMLHTTP")
-	} 	
-	catch (e){
-	    try{
-		page_request = new ActiveXObject("Microsoft.XMLHTTP")
-	    }
-	    catch (e){}
-	}
-    }
-    else
-        return false
-    page_request.onreadystatechange=function(){
-	loadpage(page_request, containerid)
-    }
-    page_request.open('GET', url, true)
-    page_request.send(null)
-}
-
-function loadpage(page_request, containerid){
-
-    if (page_request.readyState == 4 && (page_request.status==200 || window.location.href.indexOf("http")==-1))
-        document.getElementById(containerid).innerHTML=page_request.responseText;
-}
-
-/***********************************************/
     
 function ajaxtocpage(url, containerid){
     var page_request = false
@@ -71,17 +39,42 @@ function loadtocpage(page_request, containerid){
     }
 }
 
-function ajaxtocpages(N){
-    ajaxtocpage('/_toc/' + N, 'wiki_toc');
-    document.getElementById('wrapper').style.marginLeft = '-200px';
-    document.getElementById('content').style.marginLeft = '200px';
+function ajaxinittocpages(){
+    page_toc();
+    ajaxtocpage('/_toc', 'wiki_toc');
+    document.getElementById('wrapper').style.marginLeft = '-160px';
+    document.getElementById('content').style.marginLeft = '160px';
+}
+
+function ajaxtocpages(){
+    document.getElementById('wiki_menu').style.display='inline';
+    document.getElementById('page_toc').style.display='inline';
+    document.getElementById('wiki_toc').style.display='inline';
+    document.getElementById('wrapper').style.marginLeft = '-160px';
+    document.getElementById('content').style.marginLeft = '160px';
+    document.getElementById('toggle_toc_menu').innerHTML = "Hide menu";
+    document.getElementById('toggle_toc').innerHTML = "Hide menu";
+    document.getElementById('menu_area').style.display='inline';
 }
 
 function ajaxnotocpages(){
+    document.getElementById('wiki_menu').style.display='none';
+    document.getElementById('page_toc').style.display='none';
+    document.getElementById('wiki_toc').style.display='none';
     document.getElementById('wrapper').style.marginLeft = '0';
     document.getElementById('content').style.marginLeft = '0';
+    document.getElementById('toggle_toc_menu').innerHTML = "Show menu";
+    document.getElementById('toggle_toc').innerHTML = "Show menu";
+    document.getElementById('menu_area').style.display='none';
 }
 
+function setCookie(c_name,value,expiredays)
+{
+    var exdate=new Date();
+    exdate.setDate(exdate.getDate()+expiredays);
+    document.cookie=c_name+ "=" +escape(value)+
+	((expiredays==null) ? "" : ";expires="+exdate.toGMTString());
+}
 function getCookie(c_name)
 {
     if (document.cookie.length>0) {
@@ -96,22 +89,41 @@ function getCookie(c_name)
     return ""
 }
 
-function checkTOC(N)
+function checkTOC()
 {
+    ajaxinittocpages();
     needs_toc=getCookie('witoc')
     if (needs_toc!=null && needs_toc=="1") {
-	ajaxtocpages(N);
-    } else {
+	ajaxtocpages();
+    }
+    else {
 	ajaxnotocpages();
     }
 }
 
-function toggleTOC(N)
+function toggleTOC()
 {
     needs_toc=getCookie('witoc')
     if (needs_toc!=null && needs_toc=="1") {
 	ajaxnotocpages();
-    } else {
-	ajaxtocpages(N);
+	setCookie('witoc', 0, 365);
     }
+    else {
+	ajaxtocpages();
+	setCookie('witoc', 1, 365);
+    }
+}
+
+function clearSearch() {
+	var txt = document.getElementById('searchtxt');
+	txt.style.color = 'black';
+	txt.value = '';
+}
+
+function setSearch() {
+	var txt = document.getElementById('searchtxt');
+	txt.style.color = 'gray';
+	if (txt.value == '') {
+		txt.value = 'Search';
+	}
 }
