9f59adf8a9d7ce3298507d9feeee033d0941f7d3
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2008-01-22 04:06:38 +0000

    * changed <style> and <script> generation

--- wubwikit/WikitWub.tcl
+++ wubwikit/WikitWub.tcl
@@ -330,8 +330,8 @@ namespace eval WikitWub {
     # header sent with each page
     #<meta name='robots' content='index,nofollow' />
     variable head [subst {
-	[<style> media all "@import url(/wikit.css)"]
-	[<style> media all "@import url(/dtree.css)"]
+	[<style> media all "@import url(/wikit.css);"]
+	[<style> media all "@import url(/dtree.css);"]
 	[<script> src /transclude.js]
 	[<script> src /dtree.js]
 	[<link> rel alternate type "application/rss+xml" title RSS href /rss.xml]
