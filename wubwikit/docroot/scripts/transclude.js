3fbd6a98c51ad2db4f334967d24e9a98dc8cd0a2
steve@digitalsmarties.com <steve@digitalsmarties.com@db1356ac-de30-0410-903d-bd898758cf13>
2008-01-23 02:57:43 +0000

    mods from Jos for backrefs

--- wubwikit/docroot/scripts/transclude.js
+++ wubwikit/docroot/scripts/transclude.js
@@ -38,6 +38,38 @@ function loadtocpage(page_request, containerid){
 	}
     }
 }
+    
+function ajaxbackrefspage(url, containerid){
+    var page_request = false
+    if (window.XMLHttpRequest) // if Mozilla, Safari etc
+        page_request = new XMLHttpRequest()
+    else if (window.ActiveXObject){ // if IE
+	try {
+	    page_request = new ActiveXObject("Msxml2.XMLHTTP")
+	} 	
+	catch (e){
+	    try{
+		page_request = new ActiveXObject("Microsoft.XMLHTTP")
+	    }
+	    catch (e){}
+	}
+    }
+    else
+        return false
+    page_request.onreadystatechange=function(){
+	loadbackrefspage(page_request, containerid)
+    }
+    page_request.open('GET', url, true)
+    page_request.send(null)
+}
+
+function loadbackrefspage(page_request, containerid){
+    if (page_request.readyState == 4 && (page_request.status==200 || window.location.href.indexOf("http")==-1)) {
+	if (page_request.responseText.length) {
+	    document.getElementById(containerid).innerHTML = page_request.responseText;
+	}
+    }
+}
 
 function ajaxinittocpages(){
     page_toc();
@@ -105,6 +137,12 @@ function checkTOC()
 	ajaxnotocpages();
     }
 }
+
+function getBackRefs(page,containerid)
+{
+    ajaxbackrefspage("/_ref/" + page + "?A=1", containerid)
+}
+
 
 function toggleTOC()
 {
