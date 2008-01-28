ecb242f8a41dd8e99ba4aeafae4bc9036b6fa7d8
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2008-01-28 11:51:47 +0000

    * removed cookies

--- trunk/wubwikit/docroot/scripts/transclude.js
+++ trunk/wubwikit/docroot/scripts/transclude.js
@@ -134,20 +134,47 @@ function clearCookie(name, path) {
   setCookie(name, 'cookieValue', yesterday, path);
 };
 
-// [Cookie] Gets a value from a cookie
-function getCookie(cookieName) {
-  var cookieValue = '';
-  var posName = document.cookie.indexOf(escape(cookieName) + '=');
-  if (posName != -1) {
-    var posValue = posName + (escape(cookieName) + '=').length;
-    var endPos = document.cookie.indexOf(';', posValue);
-    if (endPos != -1)
-      cookieValue = unescape(document.cookie.substring(posValue, endPos));
-    else
-      cookieValue = unescape(document.cookie.substring(posValue));
-  }
-  return (cookieValue);
-};
+// this fixes an issue with the old method, ambiguous values 
+// with this test document.cookie.indexOf( name + "=" );
+function getCookie( check_name ) {
+	// first we'll split this cookie up into name/value pairs
+	// note: document.cookie only returns name=value, not the other components
+	var a_all_cookies = document.cookie.split( ';' );
+	var a_temp_cookie = '';
+	var cookie_name = '';
+	var cookie_value = '';
+	var b_cookie_found = false; // set boolean t/f default f
+	
+	for ( i = 0; i < a_all_cookies.length; i++ )
+	{
+		// now we'll split apart each name=value pair
+		a_temp_cookie = a_all_cookies[i].split( '=' );
+		
+		
+		// and trim left/right whitespace while we're at it
+		cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
+	
+		// if the extracted name matches passed check_name
+		if ( cookie_name == check_name )
+		{
+			b_cookie_found = true;
+			// we need to handle case where cookie has no value but exists (no = sign, that is):
+			if ( a_temp_cookie.length > 1 )
+			{
+				cookie_value = unescape( a_temp_cookie[1].replace(/^\s+|\s+$/g, '') );
+			}
+			// note that in cases where cookie is initialized but no value, null is returned
+			return cookie_value;
+			break;
+		}
+		a_temp_cookie = null;
+		cookie_name = '';
+	}
+	if ( !b_cookie_found )
+	{
+		return null;
+	}
+}				
 
 function checkTOC()
 {
