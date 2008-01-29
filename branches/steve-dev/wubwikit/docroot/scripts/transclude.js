36aa7cdae2812a5b9c7a5a8df6da14c984f3e889
steve@digitalsmarties.com <steve@digitalsmarties.com@db1356ac-de30-0410-903d-bd898758cf13>
2008-01-29 02:12:59 +0000

    Created branch steve-dev

---
+++ branches/steve-dev/wubwikit/docroot/scripts/transclude.js
@@ -0,0 +1,149 @@
+/***********************************************
+ * Dynamic Ajax Content- © Dynamic Drive DHTML code library (www.dynamicdrive.com)
+ * This notice MUST stay intact for legal use
+ * Visit Dynamic Drive at http://www.dynamicdrive.com/ for full source code
+ * Based on functions taken from Dynamic Ajax Content:
+ *    ajaxpage
+ *    loadpage
+ ***********************************************/
+    
+function ajaxtocpage(url, containerid){
+    var page_request = false
+    if (window.XMLHttpRequest) // if Mozilla, Safari etc
+        page_request = new XMLHttpRequest()
+    else if (window.ActiveXObject){ // if IE
+	try {
+	    page_request = new ActiveXObject("Msxml2.XMLHTTP")
+	} 	
+	catch (e){
+	    try{
+		page_request = new ActiveXObject("Microsoft.XMLHTTP")
+	    }
+	    catch (e){}
+	}
+    }
+    else
+        return false
+    page_request.onreadystatechange=function(){
+	loadtocpage(page_request, containerid)
+    }
+    page_request.open('GET', url, true)
+    page_request.send(null)
+}
+
+function loadtocpage(page_request, containerid){
+    if (page_request.readyState == 4 && (page_request.status==200 || window.location.href.indexOf("http")==-1)) {
+	if (page_request.responseText.length) {
+	    eval(page_request.responseText);
+	}
+    }
+}
+    
+function ajaxbackrefspage(url, containerid){
+    var page_request = false
+    if (window.XMLHttpRequest) // if Mozilla, Safari etc
+        page_request = new XMLHttpRequest()
+    else if (window.ActiveXObject){ // if IE
+	try {
+	    page_request = new ActiveXObject("Msxml2.XMLHTTP")
+	} 	
+	catch (e){
+	    try{
+		page_request = new ActiveXObject("Microsoft.XMLHTTP")
+	    }
+	    catch (e){}
+	}
+    }
+    else
+        return false
+    page_request.onreadystatechange=function(){
+	loadbackrefspage(page_request, containerid)
+    }
+    page_request.open('GET', url, true)
+    page_request.send(null)
+}
+
+function loadbackrefspage(page_request, containerid){
+    if (page_request.readyState == 4 && (page_request.status==200 || window.location.href.indexOf("http")==-1)) {
+	if (page_request.responseText.length) {
+	    document.getElementById(containerid).innerHTML = page_request.responseText;
+	}
+    }
+}
+
+function ajaxinittocpages(){
+    page_toc();
+    ajaxtocpage('/_toc', 'wiki_toc');
+    document.getElementById('wrapper').style.marginLeft = '-160px';
+    document.getElementById('content').style.marginLeft = '160px';
+    document.getElementById('menu_area').style.display = 'inline';
+    document.getElementById('searchform').style.display = 'inline';
+}
+
+function ajaxtocpages(){
+    document.getElementById('wiki_menu').style.display='inline';
+    document.getElementById('page_toc').style.display='inline';
+    document.getElementById('wiki_toc').style.display='inline';
+    document.getElementById('wrapper').style.marginLeft = '-160px';
+    document.getElementById('content').style.marginLeft = '160px';
+    document.getElementById('toggle_toc').innerHTML = "Hide menu";
+    document.getElementById('menu_area').style.display='inline';
+}
+
+function ajaxnotocpages(){
+    document.getElementById('wiki_menu').style.display='none';
+    document.getElementById('page_toc').style.display='none';
+    document.getElementById('wiki_toc').style.display='none';
+    document.getElementById('wrapper').style.marginLeft = '0';
+    document.getElementById('wrapper').style.marginRight = '-5px';
+    document.getElementById('content').style.marginLeft = '5px';
+    document.getElementById('toggle_toc').innerHTML = "Show menu";
+    document.getElementById('menu_area').style.display='none';
+}
+
+function checkTOC()
+{
+    ajaxinittocpages();
+
+    needs_toc=getCookie('wiki_toc');
+    if (needs_toc==null || needs_toc=="" || needs_toc=="1") {
+	ajaxtocpages();
+    }
+    else {
+	ajaxnotocpages();
+    }
+}
+
+function getBackRefs(page,containerid)
+{
+    ajaxbackrefspage("/_ref/" + page + "?A=1", containerid)
+}
+
+
+function toggleTOC()
+{
+    needs_toc=getCookie('wiki_toc')
+    if (needs_toc==null || needs_toc=="" || needs_toc=="1") {
+	ajaxnotocpages();
+	setCookie('wiki_toc', 0, 30, "/_toc/");
+    } else {
+	ajaxtocpages();
+	setCookie('wiki_toc', 1, 30, "/_toc/");
+    }
+}
+
+function clearSearch() {
+	var txt = document.getElementById('searchtxt');
+	txt.style.color = 'black';
+	txt.value = '';
+}
+
+function setSearch() {
+	var txt = document.getElementById('searchtxt');
+	txt.style.color = 'gray';
+	if (txt.value == '') {
+		txt.value = 'Search';
+	}
+}
+
+
