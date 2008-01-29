91c9468d4fc67e7f31a89c169ba6dddf9bd3ac8c
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2008-01-29 01:07:54 +0000

    * trying to make some toc js conditional

--- trunk/wubwikit/WikitWub.tcl
+++ trunk/wubwikit/WikitWub.tcl
@@ -332,8 +332,93 @@ namespace eval WikitWub {
     # header sent with each page
     #<meta name='robots' content='index,nofollow' />
     variable head [subst {
-	[<style> media all "@import url(/_toc/wikit.css);"]
+	[<style> media all "@import url(/wikit.css);"]
 	[<style> media all "@import url(/dtree.css);"]
+	[<script> {
+	    function setCookie( name, value, expires, path, domain, secure ) 
+	    {
+		// set time, it's in milliseconds
+		var today = new Date();
+		today.setTime( today.getTime() );
+
+		/*
+		if the expires variable is set, make the correct 
+		expires time, the current script below will set 
+		it for x number of days, to make it for hours, 
+		delete * 24, for minutes, delete * 60 * 24
+		*/
+		if ( expires ) {
+		    expires = expires * 1000 * 60 * 60 * 24;
+		}
+		var expires_date = new Date( today.getTime() + (expires) );
+
+		document.cookie = name + "=" +escape( value ) +
+		( ( expires ) ? ";expires=" + expires_date.toGMTString() : "" ) + 
+		( ( path ) ? ";path=" + path : "" ) + 
+		( ( domain ) ? ";domain=" + domain : "" ) +
+		( ( secure ) ? ";secure" : "" );
+	    }
+
+	    // [Cookie] Clears a cookie
+	    function clearCookie(name, path) {
+		var now = new Date();
+		var yesterday = new Date(now.getTime() - 1000 * 60 * 60 * 24);
+		setCookie(name, 'cookieValue', yesterday, path);
+	    };
+
+	    // this fixes an issue with the old method, ambiguous values 
+	    // with this test document.cookie.indexOf( name + "=" );
+	    function getCookie( check_name ) {
+		// first we'll split this cookie up into name/value pairs
+		// note: document.cookie only returns name=value, not the other components
+		var a_all_cookies = document.cookie.split( ';' );
+		var a_temp_cookie = '';
+		var cookie_name = '';
+		var cookie_value = '';
+		var b_cookie_found = false; // set boolean t/f default f
+		
+		for ( i = 0; i < a_all_cookies.length; i++ )
+		{
+		 // now we'll split apart each name=value pair
+		 a_temp_cookie = a_all_cookies[i].split( '=' );
+		 
+		 
+		 // and trim left/right whitespace while we're at it
+		 cookie_name = a_temp_cookie[0].replace(/^\s+|\s+$/g, '');
+		 
+		 // if the extracted name matches passed check_name
+		 if ( cookie_name == check_name )
+		 {
+		     b_cookie_found = true;
+		     // we need to handle case where cookie has no value but exists (no = sign, that is):
+		     if ( a_temp_cookie.length > 1 )
+		     {
+			 cookie_value = unescape( a_temp_cookie[1].replace(/^\s+|\s+$/g, '') );
+		     }
+		     // note that in cases where cookie is initialized but no value, null is returned
+		     return cookie_value;
+		     break;
+		 }
+		 a_temp_cookie = null;
+		 cookie_name = '';
+	     }
+		if ( !b_cookie_found )
+		{
+		    return null;
+		}
+	    }				
+
+	    function checkTOC()
+	    {
+		needs_toc=getCookie('wiki_toc');
+		if (needs_toc==null) {
+		    setCookie('wiki_toc', 0, 30, "/_toc/");
+		} elseif (needs_toc=="" || needs_toc=="1") {
+		} else {
+		}
+	    }
+	}]
+
 	[<script> src /_toc/transclude.js]
 	[<script> src /_toc/dtree.js]
 	[<link> rel alternate type "application/rss+xml" title RSS href /rss.xml]
@@ -1865,8 +1950,8 @@ proc Responder::post {rsp} {
 proc Incoming {req} {
 
     #dict set req -cookies [Cookies parse4server [Dict get? $req cookie]]
-    #set req [Cookies 4Server $req]
-    set req [Session fetch $req -path /_edit/]
+    set req [Cookies 4Server $req]
+    #set req [Session fetch $req -path /_edit/]
 
     if {[dict exists $req -session]} {
 	# do something with existing session
@@ -1950,12 +2035,16 @@ proc Incoming {req} {
 	    ::bin do $req
 	}
 
-	/_toc/*.js {
-	    # silently redirect js files
-	    #set toc_flag [Cookie fetchAll [dict exists $req -cookies] -name wiki_toc]
-	    dict set req -suffix [file tail [dict get $req -path]]
-	    ::scripts do $req
-	}
+       /_toc/*.js {
+            # silently redirect js files
+            Debug.error {cookies: [Dict get? $req -cookies] / [Dict get? $req cookies]}
+            if {[Dict get? $req -cookies] eq {}} {
+                Http NotFound $req
+            } else {
+                dict set req -suffix [file tail [dict get $req -path]] 
+                ::scripts do $req 
+            }
+        }
 
 	/robots.txt -
 	/*.js {
