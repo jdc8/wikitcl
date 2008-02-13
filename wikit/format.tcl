03128ec09d0616113e5ab2aa822e1a18f2de6d38
jos.decoster <jos.decoster@db1356ac-de30-0410-903d-bd898758cf13>
2008-02-13 15:18:15 +0000

    Small changes to url detection and new url format <URL:your url here>

--- wikit/format.tcl
+++ wikit/format.tcl
@@ -571,7 +571,7 @@ namespace eval Wikit::Format {
     # Compat: (Bugfix) Added " to the regexp as proper boundary of an url.
     #set re {\m(https?|ftp|news|mailto|file):(\S+[^\]\)\s\.,!\?;:'>"])}
     #set re {\m(https?|ftp|news|mailto|file):([^\s:]+[^\]\)\s\.,!\?;:'>"])}
-    set re {\m(https?|ftp|news|mailto|file):([^\s:]\S*[^\]\)\s\.,!\?;:'>"])} ;#"
+    set re {\m(https?|ftp|news|mailto|file):([^\s:]+?\S*?[^\]\)\s\.,!\?;:'>"]?)} ;#"
     set txt 0
     set end [string length $text]
 
@@ -622,8 +622,11 @@ namespace eval Wikit::Format {
     set bpre  {\[(brefs:)([^\]]*)]}  ; #  page back-references ; # compat
     #set lre  {\m(https?|ftp|news|mailto|file):(\S+[^\]\)\s\.,!\?;:'>"])} ; # "
     #set lre  {\m(https?|ftp|news|mailto|file):([^\s:]+[^\]\)\s\.,!\?;:'>"])} ; # "
-    set lre  {\m(https?|ftp|news|mailto|file):([^\s:]\S*[^\]\)\s\.,!\?;:'>"])} ; # "
-    set lre2 {\m(https?|ftp|news|mailto|file):([^\s:]\S*[^\]\)\s\.,!\?;:'>"]%\|%[^%]+%\|%)} ; # "
+#   set lre  {\m(https?|ftp|news|mailto|file):([^\s:]\S*[^\]\)\s\.,!\?;:'>"])} ; # "
+    set lre  {\m(https?|ftp|news|mailto|file):([^\s:]+?\S*?[^\]\)\s\.,!\?;:'>"]?)} ; # "
+#   set lre2 {\m(https?|ftp|news|mailto|file):([^\s:]\S*[^\]\)\s\.,!\?;:'>"]%\|%[^%]+%\|%)} ; # "
+    set lre2 {\m(https?|ftp|news|mailto|file):([^\s:]+?\S*?[^\]\)\s\.,!\?;:'>"]?%\|%[^%]+?%\|%)} ; # "
+    set lre3 {<URL:(https?|ftp|news|mailto|file):([^>]*?)>}
 
     set blre "\\\[\0\1u\2(\[^\0\]*)\0\\\]"
 
@@ -653,6 +656,7 @@ namespace eval Wikit::Format {
                                                  ## puts stderr A>>$text<<*
 
                                                  # Isolate external links.
+                                                 regsub -all $lre3 $text "\0\1u\2\\1\3\\2\0" text
                                                  regsub -all $lre2 $text "\0\1u\2\\1\3\\2\0" text
                                                  regsub -all $lre  $text "\0\1u\2\\1\3\\2\0" text
                                                  set text [string map {\3 :} $text]
