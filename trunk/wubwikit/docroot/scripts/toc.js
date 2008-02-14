acc83bc6a539e630787d2019f18078cc1d5631ac
jos.decoster <jos.decoster@db1356ac-de30-0410-903d-bd898758cf13>
2008-02-14 14:58:49 +0000

    Splitted transclude.js

--- trunk/wubwikit/docroot/scripts/toc.js
+++ trunk/wubwikit/docroot/scripts/toc.js
@@ -39,38 +39,6 @@ function loadtocpage(page_request, containerid){
     }
 }
     
-function ajaxbackrefspage(url, containerid){
-    var page_request = false
-    if (window.XMLHttpRequest) // if Mozilla, Safari etc
-        page_request = new XMLHttpRequest()
-    else if (window.ActiveXObject){ // if IE
-	try {
-	    page_request = new ActiveXObject("Msxml2.XMLHTTP")
-	} 	
-	catch (e){
-	    try{
-		page_request = new ActiveXObject("Microsoft.XMLHTTP")
-	    }
-	    catch (e){}
-	}
-    }
-    else
-        return false
-    page_request.onreadystatechange=function(){
-	loadbackrefspage(page_request, containerid)
-    }
-    page_request.open('GET', url, true)
-    page_request.send(null)
-}
-
-function loadbackrefspage(page_request, containerid){
-    if (page_request.readyState == 4 && (page_request.status==200 || window.location.href.indexOf("http")==-1)) {
-	if (page_request.responseText.length) {
-	    document.getElementById(containerid).innerHTML = page_request.responseText;
-	}
-    }
-}
-
 function ajaxinittocpages(){
     page_toc();
     ajaxtocpage('/_toc', 'wiki_toc');
@@ -187,12 +155,6 @@ function checkTOC()
     }
 }
 
-function getBackRefs(page,containerid)
-{
-    ajaxbackrefspage("/_ref/" + page + "?A=1", containerid)
-}
-
-
 function toggleTOC()
 {
     needs_toc=getCookie('wiki_toc')
@@ -203,102 +165,4 @@ function toggleTOC()
 	ajaxtocpages();
 	setCookie('wiki_toc', 1, 30, "/_toc/");
     }
-}
-
-function clearSearch() {
-	var txt = document.getElementById('searchtxt');
-	txt.style.color = 'black';
-	txt.value = '';
-}
-
-function setSearch() {
-	var txt = document.getElementById('searchtxt');
-	txt.style.color = 'gray';
-	if (txt.value == '') {
-		txt.value = 'Search titles';
-	}
-}
-
-function clearGoogle() {
-	var txt = document.getElementById('googletxt');
-	txt.style.color = 'black';
-	txt.value = '';
-}
-
-function setGoogle() {
-	var txt = document.getElementById('googletxt');
-	txt.style.color = 'gray';
-	if (txt.value == '') {
-		txt.value = 'Search in pages';
-	}
-}
-
-function URLencode(sStr) {
-    return escape(sStr)
-	.replace(/\+/g, '%2B')
-	.replace(/\"/g,'%22')
-	.replace(/\'/g, '%27');
-}
-
-function App(query) {
-    this.query = query;
-    this.resultCount = 0;
-    this.siteSearch = new GwebSearch();
-    this.siteSearch.setUserDefinedLabel("Tcler's wiki");
-    this.siteSearch.setUserDefinedClassSuffix("siteSearch");
-    this.siteSearch.setSiteRestriction("http://wiki.tcl.tk");
-    this.siteSearch.setResultSetSize(GSearch.LARGE_RESULTSET);
-    this.siteSearch.setSearchCompleteCallback(this, App.prototype.OnSearchComplete);
-    document.getElementById("page_toc").innerHTML = "";
-    document.title = "Search";
-    document.getElementById("title").innerHTML = "Search";
-    document.getElementById("updated").innerHTML = "";
-    document.getElementById("wiki_menu").innerHTML = "<ul id='menu'><li><a href='http://wiki.tcl.tk'>Home</a></li><li><a href='/4'>Recent changes</a></li><li><a href='/3'>Help</a></li></ul>";
-    document.getElementById("footer").innerHTML = "<a href='http://wiki.tcl.tk'>Home</a> &bull; <a href='/4'>Recent changes</a> &bull; <a href='/3'>Help</a> &bull; <a href='/2'>Search</a>";
-    document.getElementById("content").innerHTML = "<div id='branding'>Powered by google</div>";
-    GSearch.getBranding(document.getElementById("branding"));
-    document.getElementById("content").innerHTML += "<p><div id='searchprogress'>Searching for &quot;<b>" + query + "</b>&quot;...</div></p>";
-    this.siteSearch.execute(query);
-}
-
-App.prototype.OnSearchComplete = function() {
-    var eos = 0;
-    if (this.siteSearch.results && this.siteSearch.results.length > 0) {
-	document.getElementById("content").innerHTML += "<ul>";
-	for (var i = 0; i < this.siteSearch.results.length; i++) {
-            var result = this.siteSearch.results[i];
-	    try {
-		var h = "<li class='result'><a href='" + result.url + "'>" + result.title + "</a><p class='result'>" + result.content + "</p></li>";
-		document.getElementById("content").innerHTML += h;
-		this.resultCount++;
-	    }
-	    catch(err) {
-	    }
-	}	
-	document.getElementById("content").innerHTML += "</ul>";
-	var cursor = this.siteSearch.cursor;
-	if (cursor && cursor.currentPageIndex < cursor.pages.length - 1) {
-	    this.siteSearch.gotoPage(cursor.currentPageIndex + 1);
-	}
-	else
-	    eos = 1;
-    }
-    else
-	eos = 1;
-    
-    if (eos) {
-	var googleQuery = this.query + " site:http://wiki.tcl.tk";
-	if (this.resultCount > 0) {
-	    document.getElementById("searchprogress").innerHTML = "Search results for &quot;<b>" + this.query + "</b>&quot;:";
-	    document.getElementById("content").innerHTML += "<p><a class='googlesearch' target='_blank' href='http://www.google.com/search?q=" + URLencode(googleQuery) + "'>Click here to see all matches on Google Web Search</a></p>";
-	}
-	else {
-	    document.getElementById("searchprogress").innerHTML = "No search results for &quot;<b>" + this.query + "</b>&quot;.";
-	}
-    }
-}
-
-function googleQuery() {
-    var app = new App(document.getElementById("googletxt").value);
-    return false;
 }
