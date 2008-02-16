a286bae432fd38acfd6d2b14fb6b26c4f0693ffc
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2008-02-16 07:01:24 +0000

    * merged TOC changes from jos-dev

--- trunk/wubwikit/docroot/scripts/search.js
+++ trunk/wubwikit/docroot/scripts/search.js
@@ -8,21 +8,7 @@ function setSearch() {
 	var txt = document.getElementById('searchtxt');
 	txt.style.color = 'gray';
 	if (txt.value == '') {
-		txt.value = 'Search titles';
-	}
-}
-
-function clearGoogle() {
-	var txt = document.getElementById('googletxt');
-	txt.style.color = 'black';
-	txt.value = '';
-}
-
-function setGoogle() {
-	var txt = document.getElementById('googletxt');
-	txt.style.color = 'gray';
-	if (txt.value == '') {
-		txt.value = 'Search in pages';
+		txt.value = 'Search';
 	}
 }
 
@@ -42,15 +28,7 @@ function App(query) {
     this.siteSearch.setSiteRestriction("http://wiki.tcl.tk");
     this.siteSearch.setResultSetSize(GSearch.LARGE_RESULTSET);
     this.siteSearch.setSearchCompleteCallback(this, App.prototype.OnSearchComplete);
-    document.getElementById("page_toc").innerHTML = "";
-    document.title = "Search";
-    document.getElementById("title").innerHTML = "Search";
-    document.getElementById("updated").innerHTML = "";
-    document.getElementById("wiki_menu").innerHTML = "<ul id='menu'><li><a href='http://wiki.tcl.tk'>Home</a></li><li><a href='/4'>Recent changes</a></li><li><a href='/3'>Help</a></li></ul>";
-    document.getElementById("footer").innerHTML = "<a href='http://wiki.tcl.tk'>Home</a> &bull; <a href='/4'>Recent changes</a> &bull; <a href='/3'>Help</a> &bull; <a href='/2'>Search</a>";
-    document.getElementById("content").innerHTML = "<div id='branding'>Powered by google</div>";
-    GSearch.getBranding(document.getElementById("branding"));
-    document.getElementById("content").innerHTML += "<p><div id='searchprogress'>Searching for &quot;<b>" + query + "</b>&quot;...</div></p>";
+    document.getElementById("content").innerHTML = "<p><div id='searchprogress'>Searching for &quot;<b>" + query + "</b>&quot;...</div></p>";
     this.siteSearch.execute(query);
 }
 
@@ -61,9 +39,15 @@ App.prototype.OnSearchComplete = function() {
 	for (var i = 0; i < this.siteSearch.results.length; i++) {
             var result = this.siteSearch.results[i];
 	    try {
-		var h = "<li class='result'><a href='" + result.url + "'>" + result.title + "</a><p class='result'>" + result.content + "</p></li>";
-		document.getElementById("content").innerHTML += h;
-		this.resultCount++;
+		var idx = result.url.lastIndexOf("/");
+		var page = "";
+		if (idx >= 0)
+		    page = result.url.substr(idx);
+		if (page != "/4") {
+		    var h = "<li class='result'><a href='" + result.url + "'>" + result.title + "</a><p class='result'>" + result.content + "</p></li>";
+		    document.getElementById("content").innerHTML += h;
+		    this.resultCount++;
+		}
 	    }
 	    catch(err) {
 	    }
@@ -82,16 +66,18 @@ App.prototype.OnSearchComplete = function() {
     if (eos) {
 	var googleQuery = this.query + " site:http://wiki.tcl.tk";
 	if (this.resultCount > 0) {
-	    document.getElementById("searchprogress").innerHTML = "Search results for &quot;<b>" + this.query + "</b>&quot;:";
-	    document.getElementById("content").innerHTML += "<p><a class='googlesearch' target='_blank' href='http://www.google.com/search?q=" + URLencode(googleQuery) + "'>Click here to see all matches on Google Web Search</a></p>";
+	    document.getElementById("searchprogress").innerHTML = "Search results for &quot;<b>" + this.query + "</b>&quot;";
+	    document.getElementById("content").innerHTML += "<p><a class='googlesearch' target='_blank' href='http://www.google.com/search?q=" + URLencode(googleQuery) + "'>Click here to see all matches on Google Web Search</a></p>"
 	}
 	else {
-	    document.getElementById("searchprogress").innerHTML = "No search results for &quot;<b>" + this.query + "</b>&quot;.";
+	    document.getElementById("searchprogress").innerHTML = "No search results for &quot;<b>" + this.query + "</b>&quot;";
 	}
     }
 }
 
 function googleQuery() {
-    var app = new App(document.getElementById("googletxt").value);
+    var app = new App(document.getElementById("gsearchtxt").value);
     return false;
 }
+
+
