3265039ce0ee60d2939ad912954e8159273c9716
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2008-02-17 09:34:14 +0000

    * added a new field to change history recording the magnitude of change.  Useful for calculating change significance.

--- trunk/wubwikit/WikitRss.tcl
+++ trunk/wubwikit/WikitRss.tcl
@@ -126,7 +126,7 @@ namespace eval WikitRss {
 	    Debug.rss {detail $name $date $who $page} 7
 
 	    if {$delta > 0} {
-		append contents [item $name $date $who $baseUrl$page "$delta lines"] \n
+		append contents [item $name $date $who $baseUrl$page "($delta characters)"] \n
 		if {[incr i] > $MaxItems} break	;# limit RSS size
 	    }
 	}
