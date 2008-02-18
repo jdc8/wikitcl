9dda5d62f0bdb019dc1d43ba5b61f2b7659090ae
jos.decoster <jos.decoster@db1356ac-de30-0410-903d-bd898758cf13>
2008-02-18 09:41:11 +0000

    Removed lines added for merge tests

--- branches/2search_boxes/wubwikit/WikitWub.tcl
+++ branches/2search_boxes/wubwikit/WikitWub.tcl
@@ -2310,6 +2310,3 @@ Debug off socket 10
 #### Source local config script (not under version control)
 catch {source [file join [file dirname [info script]] local.tcl]} r eo
 Debug.log {RESTART: [clock format [clock second]] '$r' ($eo)}
-
-# A test line for merging.
-# Another test line for merging.
