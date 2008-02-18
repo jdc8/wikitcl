2f08b8f7eca4593eea1f179b3dbdae6550827ca7
jos.decoster <jos.decoster@db1356ac-de30-0410-903d-bd898758cf13>
2008-02-18 09:26:38 +0000

    Added a comment to test merging

--- wubwikit/WikitWub.tcl
+++ wubwikit/WikitWub.tcl
@@ -2299,3 +2299,5 @@ Debug off socket 10
 #### Source local config script (not under version control)
 catch {source [file join [file dirname [info script]] local.tcl]} r eo
 Debug.log {RESTART: [clock format [clock second]] '$r' ($eo)}
+
+# A test line for merging.
