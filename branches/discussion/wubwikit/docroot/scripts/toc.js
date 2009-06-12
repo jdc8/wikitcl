4d17cd762789713b328cff2b68176ebc88d4c5b3
jos.decoster <jos.decoster@db1356ac-de30-0410-903d-bd898758cf13>
2009-06-12 10:19:51 +0000

    Refactor edit quick reference

--- branches/discussion/wubwikit/docroot/scripts/toc.js
+++ branches/discussion/wubwikit/docroot/scripts/toc.js
@@ -4,7 +4,6 @@ function checkTOC()
     // Hide help on edit page
     try {
         document.getElementById('helptext').style.display = 'none';
-	document.getElementById("editarea").rows=35;
     } catch (e) {}
     try {
 	document.getElementById("googletxt").value;
@@ -16,13 +15,11 @@ function checkTOC()
 function editHelp()
 {
     document.getElementById('helptext').style.display='inline';
-    document.getElementById("editarea").rows=30;
     return false;
 }
 
 function hideEditHelp()
 {
     document.getElementById('helptext').style.display='none';
-    document.getElementById("editarea").rows=35;
     return false;
 }
