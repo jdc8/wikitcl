4a5297c6109ed8953c536da4645935ed584dbb09
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2009-10-09 20:14:34 +0000

    * moved wikit/Format.tcl to WFormat.tcl * renamed WikitDb to WDB

--- trunk/wubwikit/pkgIndex.tcl
+++ trunk/wubwikit/pkgIndex.tcl
@@ -1,6 +1,6 @@
 # Manually created, package setup
 
 package ifneeded WikitWub 1.0 [list source [file join $dir WikitWub.tcl]]
-package ifneeded WikitDb 1.0 [list source [file join $dir WikitDb.tcl]]
+package ifneeded WDB 1.0 [list source [file join $dir WDB.tcl]]
 package ifneeded WikitRss 1.0 [list source [file join $dir WikitRss.tcl]]
 package ifneeded WFormat 1.1 [list source [file join $dir WFormat.tcl]]
