1d8c8c7d783f98ad268b15015fa4b81ec82690cd
mcccol <mcccol@db1356ac-de30-0410-903d-bd898758cf13>
2009-12-22 00:58:00 +0000

    * made ie_onload1.JS have correct path

--- wubwikit/WikitWub.tcl
+++ wubwikit/WikitWub.tcl
@@ -614,7 +614,7 @@ namespace eval WikitWub {
 	    /* for Internet Explorer */
 	    /*@cc_on @*/
 	    /*@if (@_win32)
-	    document.write("<script defer src='%JP%/ie_onload1.JS'><\/script>");
+	    document.write("<script defer src='/%JP%/ie_onload1.JS'><\/script>");
 	    /*@end @*/
 	    
 	    /* for other browsers */
